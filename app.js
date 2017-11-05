786480ef92824ed2515374563279ca44185d33b1
ruslanzharkov <rus_basket_kg@inbox.ru>
2017-11-05 17:11:09 +0600

    Added signup with passport

--- app.js
+++ app.js
@@ -6,7 +6,10 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var expressHbs = require('express-handlebars');
 var mongoose = require('mongoose');
-var session  =require('express-session');
+var session  = require('express-session');
+var passport = require('passport');
+var flash = require('connect-flash');
+
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -14,6 +17,7 @@ var users = require('./routes/users');
 var app = express();
 
 mongoose.connect('localhost:27017/shopping');
+require('./config/passport');
 
 // view engine setup
 app.engine('.hbs', expressHbs({defaultLayout: 'layout', extname: '.hbs'}));
@@ -26,6 +30,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(session({secret: 'secret', resave: false, saveUninitialized: false}));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,38 @@
 var express = require('express');
 var router = express.Router();
-var Product = require('../models/product');
 var csrf = require('csurf');
+var passport = require('passport');
+
+var Product = require('../models/product');
 
 var csrfProtection = csrf();
 router.use(csrfProtection);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  Product.find(function(err, docs){
-    var productChunks = [];
-    var chunkSize = 3;
-    for (var i = 0; i < docs.length; i += chunkSize) {
-      productChunks.push(docs.slice(i, i  + chunkSize));
-    }
-    res.render('shop/index', { title: 'Shopping cart', products: productChunks });
-  });
+    Product.find(function(err, docs){
+        var productChunks = [];
+        var chunkSize = 3;
+        for (var i = 0; i < docs.length; i += chunkSize) {
+          productChunks.push(docs.slice(i, i  + chunkSize));
+        }
+        res.render('shop/index', { title: 'Shopping cart', products: productChunks });
+    });
 });
 
 router.get('/user/signup', function (req, res, next) {
-  res.render('user/signup', {csrfToken: req.csrfToken()});
+    var messages = req.flash('error');
+    res.render('user/signup', {csrfToken: req.csrfToken(), messages: messages, hasErrors: messages.length > 0});
 });
 
-router.post('/user/signup', function (req, res, next) {
-  res.redirect('/');
+router.post('/user/signup', passport.authenticate('local.signup', {
+    successRedirect: '/user/profile',
+    failureRedirect: '/user/signup',
+    failureFlash: true
+}));
+
+router.get('/user/profile', function (req, res, next) {
+    res.render('user/profile');
 });
 
 module.exports = router;
