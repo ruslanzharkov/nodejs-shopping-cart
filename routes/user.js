0c0301377246ffecb1d5879a56c6c37b55979ffe
ruslanzharkov <rus_basket_kg@inbox.ru>
2017-11-05 19:39:50 +0600

    Added protection access for user profile if user is authenticated

---
+++ routes/user.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var router = express.Router();
+var csrf = require('csurf');
+var passport = require('passport');
+
+var csrfProtection = csrf();
+router.use(csrfProtection);
+
+router.get('/signup', function (req, res, next) {
+    var messages = req.flash('error');
+    res.render('user/signup', {csrfToken: req.csrfToken(), messages: messages, hasErrors: messages.length > 0});
+});
+
+router.post('/signup', passport.authenticate('local.signup', {
+    successRedirect: '/user/profile',
+    failureRedirect: '/user/signup',
+    failureFlash: true
+}));
+
+router.get('/signin', function (req, res, next) {
+    var messages = req.flash('error');
+    res.render('user/signin', {csrfToken: req.csrfToken(), messages: messages, hasErrors: messages.length > 0});
+});
+
+router.post('/signin', passport.authenticate('local.signin', {
+    successRedirect: '/user/profile',
+    failureRedirect: '/user/signin',
+    failureFlash: true
+}));
+
+router.get('/logout', function (req, res, next) {
+    req.logout();
+    res.redirect('/');
+});
+
+router.get('/profile', isLoggedIn ,function (req, res, next) {
+    res.render('user/profile');
+});
+
+module.exports = router;
+
+function isLoggedIn(req, res, next) {
+    if(req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/');
+}
