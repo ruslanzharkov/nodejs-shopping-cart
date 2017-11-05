55b11cb880490f84e924bf73fb40d812df9cd84d
ruslanzharkov <rus_basket_kg@inbox.ru>
2017-11-05 19:31:42 +0600

    Deleted route with prefix '/user'

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var flash = require('connect-flash');
 var validator = require('express-validator');
 
 var index = require('./routes/index');
+var userRoutes = require('./routes/user');
 
 var app = express();
 
@@ -35,6 +36,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/user', userRoutes);
 app.use('/', index);
 
 // catch 404 and forward to error handler
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,8 @@
 var express = require('express');
 var router = express.Router();
-var csrf = require('csurf');
-var passport = require('passport');
 
 var Product = require('../models/product');
 
-var csrfProtection = csrf();
-router.use(csrfProtection);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -20,29 +16,4 @@ router.get('/', function(req, res, next) {
     });
 });
 
-router.get('/user/signup', function (req, res, next) {
-    var messages = req.flash('error');
-    res.render('user/signup', {csrfToken: req.csrfToken(), messages: messages, hasErrors: messages.length > 0});
-});
-
-router.post('/user/signup', passport.authenticate('local.signup', {
-    successRedirect: '/user/profile',
-    failureRedirect: '/user/signup',
-    failureFlash: true
-}));
-
-router.get('/user/profile', function (req, res, next) {
-    res.render('user/profile');
-});
-
-router.get('/user/signin', function (req, res, next) {
-    var messages = req.flash('error');
-    res.render('user/signin', {csrfToken: req.csrfToken(), messages: messages, hasErrors: messages.length > 0});
-});
-
-router.post('/user/signin', passport.authenticate('local.signin', {
-    successRedirect: '/user/profile',
-    failureRedirect: '/user/signin',
-    failureFlash: true
-}));
 module.exports = router;
