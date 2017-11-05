1e92e64d7b6cc5dc0d40ecdf003e7dc0b973b028
ruslanzharkov <rus_basket_kg@inbox.ru>
2017-11-05 17:43:23 +0600

    Added validation for user signup

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var mongoose = require('mongoose');
 var session  = require('express-session');
 var passport = require('passport');
 var flash = require('connect-flash');
-
+var validator = require('express-validator');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -28,6 +28,7 @@ app.set('view engine', '.hbs');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(validator());
 app.use(cookieParser());
 app.use(session({secret: 'secret', resave: false, saveUninitialized: false}));
 app.use(flash());
--- config/passport.js
+++ config/passport.js
@@ -17,6 +17,16 @@ passport.use('local.signup', new LocalStrategy({
     passwordField: 'password',
     passReqToCallback: true
 }, function (req, email, password, done) {
+    req.checkBody('email', 'Invalid email').notEmpty().isEmail();
+    req.checkBody('password', 'Invalid password').notEmpty().isLength({min: 4});
+    var errors = req.validationErrors();
+    if (errors) {
+        var messages = [];
+        errors.forEach(function (error) {
+           messages.push(error.msg);
+        });
+        return done(null, false, req.flash('error', messages));
+    }
     User.findOne({'email': email}, function (err, user) {
         if(err) {
             return done(err);
