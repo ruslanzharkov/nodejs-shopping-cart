2b99b3b5170bf8435950907863d382bdd9654b28
ruslanzharkov <rus_basket_kg@inbox.ru>
2017-11-05 21:54:13 +0600

    Added cart methods for add products to shopping cart

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 var Product = require('../models/product');
-
+var Cart = require('../models/cart');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -18,7 +18,17 @@ router.get('/', function(req, res, next) {
 
 router.get('/add-to-cart/:id', function (req, res) {
     var productId = req.params.id;
-    
+    var cart = new Cart(req.session.cart ? req.session.cart : {});
+
+    Product.findById(productId, function (err, product) {
+        if(err) {
+            return res.redirect('/');
+        }
+        cart.add(product, product.id);
+        req.session.cart = cart;
+        console.log(req.session.cart);
+        res.redirect('/');
+    })
 });
 
 module.exports = router;
