80e434db33166951e30b1af9775c503ec279ce3e
ruslanzharkov <rus_basket_kg@inbox.ru>
2017-11-06 21:34:33 +0600

    Added remove one item in shopping cart

--- models/cart.js
+++ models/cart.js
@@ -14,6 +14,17 @@ module.exports = function Cart(oldCart) {
         this.totalPrice += storedItem.item.price;
     };
 
+    this.reduceByOne = function (id) {
+        this.items[id].qty--;
+        this.items[id].price -= this.items[id].item.price;
+        this.totalQty--;
+        this.totalPrice -= this.items[id].item.price;
+
+        if(this.items[id].qty <= 0) {
+            delete this.items[id];
+        }
+    };
+
     this.generateArray = function () {
         var arr = [];
         for (var id in this.items) {
--- routes/index.js
+++ routes/index.js
@@ -33,6 +33,14 @@ router.get('/add-to-cart/:id', function (req, res) {
     })
 });
 
+router.get('/reduce/:id', function (req, res, next) {
+    var productId = req.params.id;
+    var cart = new Cart(req.session.cart ? req.session.cart : {});
+    cart.reduceByOne(productId);
+    req.session.cart = cart;
+    res.redirect('/shopping-cart');
+});
+
 router.get('/shopping-cart', function (req, res, next) {
     if(!req.session.cart) {
         return res.render('shop/shopping-cart', {products: null});
