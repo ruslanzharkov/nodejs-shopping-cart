8c73ba69526dd9e7dab2be2be790572856673034
ruslanzharkov <rus_basket_kg@inbox.ru>
2017-11-06 13:12:13 +0600

    Added cart view

--- routes/index.js
+++ routes/index.js
@@ -31,4 +31,12 @@ router.get('/add-to-cart/:id', function (req, res) {
     })
 });
 
+router.get('/shopping-cart', function (req, res, next) {
+    if(!req.session.cart) {
+        return res.render('shop/shopping-cart', {products: null});
+    }
+    var cart = new Cart(req.session.cart);
+    return res.render('shop/shopping-cart', {products: cart.generateArray(), totalPrice: cart.totalPrice});
+});
+
 module.exports = router;
