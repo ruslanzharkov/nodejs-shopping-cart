99ad6d64a850190a6cb360b5d1898e5896994b95
ruslanzharkov <rus_basket_kg@inbox.ru>
2017-11-06 21:14:21 +0600

    Added orders view in user profile

--- routes/user.js
+++ routes/user.js
@@ -3,11 +3,24 @@ var router = express.Router();
 var csrf = require('csurf');
 var passport = require('passport');
 
+var Order = require ('../models/order');
+var Cart = require ('../models/cart');
+
 var csrfProtection = csrf();
 router.use(csrfProtection);
 
 router.get('/profile', isLoggedIn, function (req, res, next) {
-    res.render('user/profile');
+    Order.find({user: req.user}, function(err, orders) {
+        if(err) {
+            return res.write('Error!');
+        }
+        var cart;
+        orders.forEach(function (order) {
+            cart = new Cart(order.cart);
+            order.items = cart.generateArray();
+        });
+        res.render('user/profile', { orders: orders });
+    });
 });
 
 router.get('/logout', isLoggedIn, function (req, res, next) {
