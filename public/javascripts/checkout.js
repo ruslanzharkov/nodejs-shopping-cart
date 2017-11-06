8059fbfb8adfac5955d2e45e191a413060ce54a3
ruslanzharkov <rus_basket_kg@inbox.ru>
2017-11-06 15:51:52 +0600

    Added checkout validation for credit card

---
+++ public/javascripts/checkout.js
@@ -0,0 +1,39 @@
+Stripe.setPublishableKey('pk_test_VXL2ZouUSiaHsJeuBFjlbJXu');
+
+var $form = $('#checkout-form');
+
+$form.submit(function(event) {
+    $('#charge-error').addClass('invisible');
+    $form.find('button').prop('disabled', true);
+    Stripe.card.createToken({
+        number: $('#card-number').val(),
+        cvc: $('#card-cvc').val(),
+        exp_month: $('#card-expiry-month').val(),
+        exp_year: $('#card-expiry-year').val(),
+        name: $('#name').val(),
+    }, stripeResponseHandler);
+    return false;
+});
+
+function stripeResponseHandler(status, response) {
+
+    if (response.error) { // Problem!
+
+        // Show the errors on the form
+        $('#charge-error').text(response.error.message);
+        $('#charge-error').removeClass('invisible');
+        $form.find('button').prop('disabled', false); // Re-enable submission
+
+    } else { // Token was created!
+
+        // Get the token ID:
+        var token = response.id;
+
+        // Insert the token into the form so it gets submitted to the server:
+        $form.append($('<input type="hidden" name="stripeToken" />').val(token));
+
+        // Submit the form:
+        $form.get(0).submit();
+
+    }
+}
